e222edc2d73ffbccbd03d0e9cea85580df51f410
Aditya <helloadig@gmail.com>
2024-09-06 11:01:59 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className="flex gap-5 ">
 
     <a href="https://github.com/AaadityaG/PearlThoughts-Task-datepicker" target="_blanck" className="text-blue-400 opacity-40">Click here to see Code on Github</a>
-    <a href="https://youtu.be/6S-yAzUMxWA" target="_blanck" className="text-blue-400 opacity-40">Click here to see the video</a>
+    {/* <a href="https://youtu.be/6S-yAzUMxWA" target="_blanck" className="text-blue-400 opacity-40">Click here to see the video</a> */}
 
     </div>
     </div>
