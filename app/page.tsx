78fb424ea9313b2fa043e99f0d2eeae71233a2f8
Aditya <helloadig@gmail.com>
2024-09-06 11:01:02 +0530

    minor changes - video link

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,12 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center gap-12 ">
     <RecurringDatePicker />
-    <a href="https://github.com/AaadityaG/PearlThoughts-Task-datepicker" target="_blanck" className="text-blue-400 opacity-20">Click here ot see Code on Github</a>
+    <div className="flex gap-5 ">
+
+    <a href="https://github.com/AaadityaG/PearlThoughts-Task-datepicker" target="_blanck" className="text-blue-400 opacity-40">Click here to see Code on Github</a>
+    <a href="https://youtu.be/6S-yAzUMxWA" target="_blanck" className="text-blue-400 opacity-40">Click here to see the video</a>
+
+    </div>
     </div>
   );
 }
